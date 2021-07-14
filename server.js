d4a4000a9e62c6d808830cfeedbec9b0134e8230
michaelshxw <michaelshxw@gmail.com>
2021-07-14 17:59:37 +0800

    added back end display routes with todo comments

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const fs = require('fs');
+const path = require('path')
 require('dotenv').config();
 
 const app = express();
@@ -10,12 +11,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// FRONT END DISPLAY
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// BACK END DISPLAY
+
+app.get("/api/notes", (req, res) => {
+  // read db.json
+  // return saved notes as json
+});
+
+app.post("/api/notes", (req, res) => {
+  // receive a note to save on the request body
+  // add to db.json file
+  // return the new note to the client with a unique id using uuid
+});
+
+app.delete("/api/notes:id", (req, res) => {
+  // receive a query containing the id 
+  // read all notes, remove the note with the given id
+  // rewrute the notes to the db.json file
 });
 
 app.listen(PORT, () => {
