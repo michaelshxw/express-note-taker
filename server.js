8f0df35ad823e9fa0868c09e35dc7e901050ad21
michaelshxw <michaelshxw@gmail.com>
2021-07-14 18:41:47 +0800

    add require for uuid

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path')
 require('dotenv').config();
+const uuid = require("uuid");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
